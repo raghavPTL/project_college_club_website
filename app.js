f3cc99bbaf1e05fb2de0b869bc906a672d64819e
RAGHAV PATEL <raghavnpatel@RAGHAVs-MacBook-Air.local>
2022-08-20 22:48:03 +0530

    ignore node modulls by .gitignore

--- app.js
+++ app.js
@@ -406,6 +406,10 @@ app.post("/submit",function(req,res){
 
 // listen........
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
